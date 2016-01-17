4cdfeeadc76e7b35fe4f6bc491b9ae960fbb8ba6
Randall Kanna <randallkanna@gmail.com>
2016-01-17 12:02:01 -0800

    addition of toolbar

---
+++ blocbrowser/blocbrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blocbrowser
+//
+//  Created by RandallKanna on 1/17/16.
+//  Copyright © 2016 RandallKanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (void) resetWebView;
+
+@end
